6c29031a12d71b29d9c35233d58abc02554c508c
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-29 23:21:02 +0700

    Implement receive_notfication function in Notificstion service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -92,6 +92,12 @@ impl NotificationService {
             .join().unwrap();
     }
 
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result:Notification=NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
+
+
 
 
 }
