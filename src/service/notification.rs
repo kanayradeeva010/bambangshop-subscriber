6dcf780cc1db44fc8282cf219a41616b7aaa07b1
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-29 23:26:23 +0700

    Implement list_messages function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -97,7 +97,9 @@ impl NotificationService {
         return Ok(subscriber_result);
     }
 
-
+    pub fn list_messages() -> Result<Vec<String>> {
+        return Ok(NotificationRepository:: list_all_as_string());
+    }
 
 
 }
